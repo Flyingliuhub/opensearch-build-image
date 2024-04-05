dff061dadc68e43e0055dba548fb7aa01f37344f
Tao Liu <liutaoaz@amazon.com>
2024-04-04 23:05:02 -0700

    add feature flag
    Signed-off-by: Tao Liu <liutaoaz@amazon.com>

--- scripts/components/OpenSearch-Dashboards/build.sh
+++ scripts/components/OpenSearch-Dashboards/build.sh
@@ -138,7 +138,7 @@ fi
 eval $NVM_CMD
 
 echo "Building node modules for core with $PLATFORM-$DISTRIBUTION-$ARCHITECTURE"
-yarn osd bootstrap
+yarn osd bootstrap --single-version=loose
 
 echo "Building artifact"
 
